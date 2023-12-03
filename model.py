132659a260766921d4cb0d7581b2ed2158736f1e
kirek <kirylmeleniako@gmail.com>
2023-12-03 12:04:18 +0100

    Create and build a model

---
+++ model.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis = 1)
+x_test = tf.keras.utils.normalize(x_test, axis = 1)
+
+model = Sequential([
+    Flatten(input_shape = (28, 28)),
+    Dense(128, activation = 'relu'),
+    Dense(64, activation = 'relu'),
+    Dropout(0.1),
+    Dense(10, activation = 'softmax'),
+])
+
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(x_train, y_train, epochs = 10)  
+
+model.save('digits_recognition.model')
+
